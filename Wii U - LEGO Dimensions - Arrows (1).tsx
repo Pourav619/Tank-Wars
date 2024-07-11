54eedac04b995fe9548b370b0357ec9ba66a1121
Pourav <108517421+Pourav619@users.noreply.github.com>
2024-07-12 03:20:34 +0530

    Add files via upload

---
+++ Wii U - LEGO Dimensions - Arrows (1).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Wii U - LEGO Dimensions - Arrows (1)" tilewidth="16" tileheight="16" spacing="1" margin="10" tilecount="552" columns="24">
+ <image source="assets/Wii U - LEGO Dimensions - Arrows (1).png" width="424" height="404"/>
+</tileset>
